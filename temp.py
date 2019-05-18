4330bfe9c3f579dcd3a59d03ee15bb84c967ff45
Rayssa Silva <rayssa17@astro.ufrj.br>
2019-05-17 23:00:59 -0300

    Criaçao de arquivo com informações dos headers

--- temp.py
+++ temp.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
-#from astropy.io.fits import fits
+from astropy.io import fits
 import astropy
 import glob
 import os.path
 import pandas as pd
 import os
-
+from tqdm import tqdm
+from itertools import repeat
 
 ''' Variáveis locais do pipeline '''
 
@@ -16,7 +17,6 @@ diretorio_arquivos = os.path.join(diretorio_script, 'data')
 
 
 arquivos_brutos = glob.glob(diretorio_arquivos + '/*.fits')
-
 arquivo_auxiliar_nome = 'Arquivos_auxiliares.csv'
 arquivo_auxiliar = os.path.join(diretorio_temporario, arquivo_auxiliar_nome)
 
@@ -27,14 +27,14 @@ Checa a existência do arquivo auxiliar, caso não exista, ele é criado
 Se o arquivo já existe ele é sobreescrito a fim de evitar duplicatas nas linhas
 
 '''
-if not os.path.isfile(arquivo_auxiliar): #<-------------------------------------------------------------------------- checar essa função
-	df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])	
-	df.to_csv(arquivo_auxiliar, index=False, header=None, mode = 'a')
+#if not os.path.isfile(arquivo_auxiliar): #<-------------------------------------------------------------------------- checar essa função
+#	df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])	
+#	df.to_csv(arquivo_auxiliar, index=False)
 
 ''' Cria o arquivo temporário '''
-def agrupar(aquivo_fits):
+def agrupar(arquivo_fits, arquivo_auxiliar):
 	df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])
-	hdu = astropy.io.fits.open(arquivo_fits)
+	hdu = fits.open(arquivo_fits)
 	header = hdu[0].header
 	hdu.close()
 	df.at[0, 'tipo_imagem'] = header['OBJECT']
@@ -43,7 +43,7 @@ def agrupar(aquivo_fits):
 	df.at[0, 'caminho_arquivo'] = arquivo_fits
 	''' Adiciona as informações da imagem ao arquivo '''
 	df.to_csv(arquivo_auxiliar, index=False, header=None, mode = 'a')
+	return
 
 ''' Mapeia as informações de todos os fits originais '''
-map(agrupar, arquivos_brutos)
-
+list(map(agrupar, tqdm(arquivos_brutos, ascii=False, desc='Guardando informações dos headers'), repeat(arquivo_auxiliar)))
