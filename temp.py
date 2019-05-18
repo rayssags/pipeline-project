c5e3a5c0aee2c3d73e254a408cef0aae05c156f8
Rayssa Silva <rayssa17@astro.ufrj.br>
2019-05-18 15:03:36 -0300

    Criação de masterbias e flat field

--- temp.py
+++ temp.py
@@ -7,6 +7,7 @@ import pandas as pd
 import os
 from tqdm import tqdm
 from itertools import repeat
+import numpy as np
 
 ''' Variáveis locais do pipeline '''
 
@@ -31,6 +32,11 @@ Se o arquivo já existe ele é sobreescrito a fim de evitar duplicatas nas linha
 #	df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])	
 #	df.to_csv(arquivo_auxiliar, index=False)
 
+df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])	
+df.to_csv(arquivo_auxiliar, index=False)
+
+
+
 ''' Cria o arquivo temporário '''
 def agrupar(arquivo_fits, arquivo_auxiliar):
 	df = pd.DataFrame(columns=['tipo_imagem', 'filtro', 'tempo_exposicao', 'caminho_arquivo'])
@@ -46,4 +52,47 @@ def agrupar(arquivo_fits, arquivo_auxiliar):
 	return
 
 ''' Mapeia as informações de todos os fits originais '''
-list(map(agrupar, tqdm(arquivos_brutos, ascii=False, desc='Guardando informações dos headers'), repeat(arquivo_auxiliar)))
+w = list(map(agrupar, tqdm(arquivos_brutos, ascii=False, desc='Guardando informações dos headers'), repeat(arquivo_auxiliar)))
+del w
+
+dimensoes = fits.open(arquivos_brutos[0])[0].data.shape
+arquivo_auxiliar_dados = pd.read_csv(arquivo_auxiliar)
+print('Criando bias frame')
+
+''' Criando masterbias pela mediana'''
+arquivos_bias = arquivo_auxiliar_dados.loc[arquivo_auxiliar_dados['tipo_imagem'] == 'zero', 'caminho_arquivo']
+imagens = list(map(lambda x: fits.open(x)[0].data, arquivos_bias))
+master_bias_data = np.median(imagens, axis = 0)
+master_bias_hdu = fits.PrimaryHDU(master_bias_data)
+master_bias_caminho = os.path.join(diretorio_temporario, 'masterbias.fits')
+master_bias_hdu.writeto(master_bias_caminho, overwrite=True)
+
+del master_bias_hdu
+''' Criando flat pela mediana'''
+flats = arquivo_auxiliar_dados.loc[arquivo_auxiliar_dados['tipo_imagem'] == 'flat']
+tempos_exposicao_flats = flats['tempo_exposicao'].unique()
+
+def master_flat(tempo_exposicao_flats):
+	arquivos = flats.loc[flats['tempo_exposicao'] == tempo_exposicao_flats, 'caminho_arquivo']
+	imagens = list(map(lambda x: fits.open(x)[0].data, arquivos))
+	master_flat_data = np.median(imagens, axis = 0) - master_bias_data
+	master_flat_data_normalizado = master_flat_data/np.mean(master_flat_data)
+	master_flat_hdu = fits.PrimaryHDU(master_flat_data_normalizado)
+	master_flat_caminho = os.path.join(diretorio_temporario, 'masterflat_norm_{}.fits'.format(tempo_exposicao_flats))
+	master_flat_hdu.writeto(master_flat_caminho, overwrite=True)
+	del master_flat_hdu
+
+print('Criando flat field normalizado')
+
+w = list(map(master_flat, tempos_exposicao_flats))
+del w
+
+#w = list(map(master_flat, tqdm(tempos_exposicao_flats, ascii=False, desc='Criando flat field normalizado:')))
+#del w
+
+
+'''
+Agora falta descobrir como corrigir céu
+- determinar quais regiões não tem estrelas
+- fazer fit nessas regiões
+'''
