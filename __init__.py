fa3de8af5ee6c9df0400fe2d9e4ce01ae98d9c3d
Rayssa Silva <rayssa17@astro.ufrj.br>
2019-06-16 10:32:03 -0300

    versão para terminal e documentação

---
+++ __init__.py
@@ -0,0 +1,14 @@
+"""
+Pacote de funções para a crição de um pipeline básico de redução de dados astronômicos em Python.
+Faz parte da avaliação da disciplina Tratamento de Dados Astronômicos do curso de Astronomia do Observatório do Valongo/Universidade Federal do Rio de Janeiro.
+"""
+
+__python_version__ = '3.7'
+__astropy_version__ = '3.1.2'
+__numpy_version__ = '1.16.2'
+__pandas_version__ = '0.24.2'
+
+
+__name__ = "Ceci est une pipe(line)"
+
+from .pipe_functions import *
